29e1148b2f8153bd1a07ff4228bb4ef41be7cd15
Ionizing <PeterSmith_9@outlook.com>
2020-02-03 00:54:50 +0800

    [conf.py] change theme to base-jinja

--- conf.py
+++ conf.py
@@ -155,7 +155,7 @@ NAVIGATION_ALT_LINKS = {
 }
 
 # Name of the theme to use.
-THEME = "oldfashioned"
+THEME = "base-jinja"
 
 # Primary color of your theme. This will be used to customize your theme.
 # Must be a HEX value.
